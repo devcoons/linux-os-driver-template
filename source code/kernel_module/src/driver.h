f3dd6182c27cf8db820c27554ed7ada0a5f23271
Io.D <i_-_-_s@outlook.com>
2015-11-05 23:40:26 +0200

    Update [Folders Structure]

---
+++ source code/kernel_module/src/driver.h
@@ -0,0 +1,48 @@
+#ifndef _DRIVER_H
+	#define _DRIVER_H
+	
+		#include <linux/init.h>           
+		#include <linux/module.h>         
+		#include <linux/device.h>         
+		#include <linux/kernel.h>         
+		#include <linux/fs.h>             
+		#include <asm/uaccess.h>
+	
+			#define  DEVICE_NAME "tdriver"    
+			#define  CLASS_NAME  "tdriver"        
+
+			MODULE_LICENSE("GPL");            
+			MODULE_AUTHOR("Io.D");    
+			MODULE_DESCRIPTION("Template Driver Module");  
+			MODULE_VERSION("0.1"); 
+		
+			struct drv_data_t
+			{
+				int    major_number;
+				char   message[256];
+				short  sizeof_message;
+				int    count_usage; 
+				struct class	* amm_class;	
+				struct device	* amm_device;			
+			}drv_data;
+			
+			static int 		dev_open	(	struct inode *, struct file * );
+			static int 		dev_release	(	struct inode *, struct file * );
+	
+			static ssize_t 	dev_read	(	struct file  *, char * 		 , size_t , loff_t *	);
+			static ssize_t 	dev_write	(	struct file  *, const char  *, size_t , loff_t *	);
+			
+			static struct file_operations fops =
+			{
+			   .open = dev_open,
+			   .read = dev_read,
+			   .write = dev_write,
+			   .release = dev_release,
+			};
+			
+			static int 	__init 	drv_devchar_init	(void);
+			static void __exit 	drv_devchar_exit	(void);
+			
+
+#endif
+	
